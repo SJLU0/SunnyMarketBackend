3eaf5e30aad76e076bf844b25153029a34ec5469
q12567893 <q12567893@gmail.com>
2024-12-20 17:19:26 +0800

    add:專案初始化

---
+++ src/main/java/com/example/sunnymarketbackend/SunnyMarketBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.sunnymarketbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunnyMarketBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunnyMarketBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sunnymarketbackend/SunnyMarketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sunnymarketbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunnyMarketBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
